d1edcc5a618cb0ff17e9ad3bc8eedb3a8d93b47e
homer <l.gosiewski@dook.pro>
2020-01-24 15:34:53 +0100

    Initial commit

---
+++ src/main/java/com/gosiewski/contributorsjava/ContributorsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gosiewski.contributorsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContributorsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContributorsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gosiewski/contributorsjava/ContributorsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosiewski.contributorsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContributorsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
